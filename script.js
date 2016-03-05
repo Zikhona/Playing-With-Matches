ac62b95a62c1e18d3d90833f951b9b201382379c
Zikhona <zikhonalaurettapoto35@gmail.com>
2016-03-05 15:12:21 +0200

    randomPink box

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 		// Returns a random number between 0 (inclusive) and 1 (exclusive)
 function getRandom(newRandom) {
 	// returns a random number from 1 to 9
-	var newRandom = Math.floor((Math.random() * 9) + 1);
-      return  document.getElementById("demo").innerHTML = newRandom;
+	var newRandom = Math.floor((Math.random() * 3) + 1);
+  document.getElementById("demo").innerHTML = newRandom;
+  
 }
 getRandom();
