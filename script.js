ea7d5bf61281ed952298b4c2497305f53afd0192
Zikhona <zikhonalaurettapoto35@gmail.com>
2016-03-07 10:27:07 +0200

    three pnik boxes

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-		// Returns a random number between 0 (inclusive) and 1 (exclusive)
-function getRandom(newRandom) {
-	// returns a random number from 1 to 9
-	var newRandom = Math.floor((Math.random() * 3) + 1);
-  document.getElementById("demo").innerHTML = newRandom;
+// 		// Returns a random number between 0 (inclusive) and 1 (exclusive)
+// function getRandom(newRandom) {
+// 	// returns a random number from 1 to 9
+// 	var newRandom = Math.floor((Math.random() * 3) + 1);
+//   document.getElementById("demo").innerHTML = newRandom;
   
+// }
+// getRandom();
+
+
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandom();
+  document.getElementById("demo").innerHTML = getRandomIntInclusive(1, 3);
+  document.getElementById("demo2").innerHTML = getRandomIntInclusive(1, 3);
+  document.getElementById("demo3").innerHTML = getRandomIntInclusive(1, 3);
+
